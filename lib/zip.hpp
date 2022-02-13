b5f52334a002453c4edc8fd731a5d4d80b91c15a
Evgenii Balabanov <e.balabanov@apollo4u.net>
2022-02-13 15:54:02 +0300

    delete ZipBase

--- lib/zip.hpp
+++ lib/zip.hpp
@@ -5,59 +5,41 @@ namespace py {
 
 namespace impl {
 
-template<typename Type>
-class ZipBase { // class ZipBase : Type {}; ???
-public:
-  using container_iterator = typename Type::iterator;
-
-  ZipBase(Type& container) : container_(container) {}
-
-  container_iterator begin() const {
-    return container_.begin();
-  }
-
-  container_iterator end() const {
-    return container_.end();
-  }
-
-private:
-  Type& container_;
-};
-
-template<typename Iterator>
+template <typename Iterator>
 class IteratorBase {
-public:
+ public:
   IteratorBase(Iterator iterator) : iterator_(iterator) {}
 
   bool operator!=(const IteratorBase& other) const {
     return iterator_ != other.iterator_;
   }
 
-  auto operator*() const {
-    return std::tie(*iterator_);
-  }
+  auto operator*() const { return std::tie(*iterator_); }
 
   IteratorBase& operator++() {
     ++iterator_;
     return *this;
   }
 
-private:
+ private:
   Iterator iterator_;
 };
 
-template<typename Head, typename ...Iterators>
+template <typename Head, typename... Iterators>
 class iterator : private IteratorBase<Head>, private iterator<Iterators...> {
-public:
-  iterator(Head head, Iterators ...iterators) : iterator<Iterators...>(iterators...), IteratorBase<Head>(head) {};
+ public:
+  iterator(Head head, Iterators... iterators)
+      : iterator<Iterators...>(iterators...), IteratorBase<Head>(head){};
 
   bool operator!=(const iterator& other) const {
     const auto& derived = *dynamic_cast<const iterator<Iterators...>*>(&other);
-    return IteratorBase<Head>::operator!=(other) || iterator<Iterators...>::operator!=(derived);
+    return IteratorBase<Head>::operator!=(other) &&
+           iterator<Iterators...>::operator!=(derived);
   }
 
   auto operator*() const {
-    return std::tuple_cat(IteratorBase<Head>::operator*(), iterator<Iterators...>::operator*());
+    return std::tuple_cat(IteratorBase<Head>::operator*(),
+                          iterator<Iterators...>::operator*());
   }
 
   iterator& operator++() {
@@ -67,26 +49,27 @@ public:
   }
 };
 
-template<typename Head>
+template <typename Head>
 class iterator<Head> : public IteratorBase<Head> {
-public:
-  iterator(Head head) : IteratorBase<Head>(head) {};
+ public:
+  iterator(Head head) : IteratorBase<Head>(head){};
 };
 
 }  // namespace impl
 
-template<typename ...Containers>
-class zip : private impl::ZipBase<Containers>... {
-public:
-  zip(Containers& ...containers) : impl::ZipBase<Containers>(containers)... {}
+template <typename... Containers>
+class zip : private std::reference_wrapper<Containers>... {
+ public:
+  zip(Containers&... containers)
+      : std::reference_wrapper<Containers>(std::ref(containers))... {}
 
   auto begin() {
-    return impl::iterator(impl::ZipBase<Containers>::begin()...);
+    return impl::iterator(std::reference_wrapper<Containers>::get().begin()...);
   }
 
   auto end() {
-    return impl::iterator(impl::ZipBase<Containers>::end()...);
+    return impl::iterator(std::reference_wrapper<Containers>::get().end()...);
   }
 };
 
-} // namespace py
+}  // namespace py
